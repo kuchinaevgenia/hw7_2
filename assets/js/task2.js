9d1170d5267bc2ea66ff6da7a51ff97d69e946bc
kuchinaevgenia <kucinaevgenia6@gmail.com>
2026-08-11 21:22:53 +0300

    Add function for task2

---
+++ assets/js/task2.js
@@ -0,0 +1,8 @@
+function checkMultiplicity(a, b) {
+  return a % b === 0;
+}
+
+console.log(checkMultiplicity(25, 5));
+console.log(checkMultiplicity(15, 3));
+console.log(checkMultiplicity(15, 5));
+console.log(checkMultiplicity(15, 4));
