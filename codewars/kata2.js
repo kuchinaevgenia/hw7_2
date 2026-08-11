4e0642bd010a24d857fa2f757c30644c05b72678
kuchinaevgenia <kucinaevgenia6@gmail.com>
2026-08-11 21:00:24 +0300

    Complete kata2

---
+++ codewars/kata2.js
@@ -0,0 +1,16 @@
+function calculate(a, o, b) {
+  if (o === "+") {
+    return a + b;
+  }
+  if (o === "-") {
+    return a - b;
+  }
+  if (o === "/" && b !== 0) {
+    return a / b;
+  }
+  if (o === "*") {
+    return a * b;
+  }
+
+  return null;
+}
